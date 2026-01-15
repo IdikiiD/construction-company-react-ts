4467f38ca915822b22ed5565b99f038614eef6ab
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-16 01:32:18 +0200

    feat: add saleData module with various sale types and their variants

---
+++ src/data/saleData.ts
@@ -0,0 +1,168 @@
+import type {SaleType} from "../../types/saleType.ts";
+
+export const saleData: SaleType[] = [
+    {
+        type: 'roof',
+        title: 'КРЫША НА БАЛКОН',
+        variants: [
+            {
+                id: 'wood',
+                title: 'Вагонка',
+                image: 'src/img/sale/roof/wood.jpg',
+                price: '600 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'plastic',
+                title: 'Пластиковая вагонка',
+                image: 'src/img/sale/roof/plastic.jpg',
+                price: '750 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'pvc',
+                title: 'ПВХ-панели',
+                image: 'src/img/sale/roof/pvc.jpg',
+                price: '800 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'laminate',
+                title: 'Настил пола из ламината',
+                image: 'src/img/sale/roof/stretch.jpg',
+                price: '1250 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'stretch',
+                title: 'Настил пола из дерева',
+                image: 'src/img/sale/roof/stretch.jpg',
+                price: '1650 ₽/кв.м.',
+                priceNote: 'с материалом',
+            }
+        ],
+    },
+    {
+        type: 'outside',
+        title: 'ВНЕШНЯЯ ОТДЕЛКА',
+        variants: [
+            {
+                id: 'wood',
+                title: 'Вагонка',
+                image: 'src/img/sale/roof/wood.jpg',
+                price: '600 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'plastic',
+                title: 'Пластиковая вагонка',
+                image: 'src/img/sale/roof/plastic.jpg',
+                price: '750 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'pvc',
+                title: 'ПВХ-панели',
+                image: 'src/img/sale/roof/pvc.jpg',
+                price: '800 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'laminate',
+                title: 'Настил пола из ламината',
+                image: 'src/img/sale/roof/stretch.jpg',
+                price: '1250 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'stretch',
+                title: 'Настил пола из дерева',
+                image: 'src/img/sale/roof/stretch.jpg',
+                price: '1650 ₽/кв.м.',
+                priceNote: 'с материалом',
+            }
+        ],
+    },
+    {
+        type: 'inside',
+        title: 'ВНУТРЕННЯЯ ОТДЕЛКА',
+        variants: [
+            {
+                id: 'wood',
+                title: 'Вагонка',
+                image: 'src/img/sale/roof/wood.jpg',
+                price: '600 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'plastic',
+                title: 'Пластиковая вагонка',
+                image: 'src/img/sale/roof/plastic.jpg',
+                price: '750 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'pvc',
+                title: 'ПВХ-панели',
+                image: 'src/img/sale/roof/pvc.jpg',
+                price: '800 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'laminate',
+                title: 'Настил пола из ламината',
+                image: 'src/img/sale/roof/stretch.jpg',
+                price: '1250 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'stretch',
+                title: 'Настил пола из дерева',
+                image: 'src/img/sale/roof/stretch.jpg',
+                price: '1650 ₽/кв.м.',
+                priceNote: 'с материалом',
+            }
+        ],
+    },
+    {
+        type: 'move-out',
+        title: 'ВЫНОСНОЕ ОСТЕКЛЕНИЕ',
+        variants: [
+            {
+                id: 'wood',
+                title: 'Вагонка',
+                image: 'src/img/sale/roof/wood.jpg',
+                price: '600 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'plastic',
+                title: 'Пластиковая вагонка',
+                image: 'src/img/sale/roof/plastic.jpg',
+                price: '750 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'pvc',
+                title: 'ПВХ-панели',
+                image: 'src/img/sale/roof/pvc.jpg',
+                price: '800 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'laminate',
+                title: 'Настил пола из ламината',
+                image: 'src/img/sale/roof/stretch.jpg',
+                price: '1250 ₽/кв.м.',
+                priceNote: 'с материалом',
+            },
+            {
+                id: 'stretch',
+                title: 'Настил пола из дерева',
+                image: 'src/img/sale/roof/stretch.jpg',
+                price: '1650 ₽/кв.м.',
+                priceNote: 'с материалом',
+            }
+        ],
+    },
+];
