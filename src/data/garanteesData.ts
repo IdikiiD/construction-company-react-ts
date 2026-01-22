aaa55f384121ab35f999f9ecd74a94af352e8e13
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-22 03:15:11 +0200

    feat: add garanteesData file for image guarantees information

---
+++ src/data/garanteesData.ts
@@ -0,0 +1,11 @@
+
+import type {ExtendedImageType} from "../../types/imageGuaranteesType.ts";
+
+export const images: ExtendedImageType[] = [
+    { id: '1', src: 'src/img/guarantees/1.png', alt: 'Photo 1',title:'Высокое качество' },
+    { id: '2', src: 'src/img/guarantees/2.png', alt: 'Photo 2', title:'Выполнение работ под ключ'},
+    { id: '3', src: 'src/img/guarantees/3.png', alt: 'Photo 3', title:'Монтаж в короткие сроки'},
+    { id: '4', src: 'src/img/guarantees/4.png', alt: 'Photo 4', title:'Цены от производителя'},
+    { id: '5', src: 'src/img/guarantees/5.png', alt: 'Photo 5', title:'Бесплатный замер и консультацию'},
+    { id: '6', src: 'src/img/guarantees/6.png', alt: 'Photo 6', title:'Тепло и уют на балконе'},
+];
