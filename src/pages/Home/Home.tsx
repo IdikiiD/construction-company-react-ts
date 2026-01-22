ec0bcfba315b304f37e959de570a4fcb6402f28d
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-22 03:15:38 +0200

    feat: update Home component to include additional sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,18 @@
 
 import {Header} from '../../sections/Header/Header.tsx';
+import {Hero} from '../../sections/Hero/Hero.tsx';
+import {Glazing} from "../../sections/Glazing/Glazing.tsx";
+import {Sale} from "../../sections/Sale/Sale.tsx";
+import {Works} from "../../sections/Works/Works.tsx";
+import {Guarantees} from "../../sections/Guarantees/Guarantees.tsx";
+import {Payment} from "../../sections/Payment/Payment.tsx";
+import {Promo} from "../../sections/Promo/Promo.tsx";
+import {Contacts} from "../../sections/Contacts/Contacts.tsx";
+import {Questions} from "../../sections/Questions/Questions.tsx";
+import {Footer} from "../../sections/Footer/Footer.tsx";
 
 export default function Home() {
 return(
-    <Header />
+    <><Header/><Hero/><Glazing/><Sale/><Works/><Guarantees/><Payment/><Promo/><Contacts/><Questions/><Footer/></>
 )
 }
