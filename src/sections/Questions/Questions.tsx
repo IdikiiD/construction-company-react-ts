f43be0d51ca14449a13e264da80f7b8c04d8a829
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-22 03:16:37 +0200

    feat: add Questions component for user inquiries

---
+++ src/sections/Questions/Questions.tsx
@@ -0,0 +1,24 @@
+export const Questions = () => {
+    return (
+        <div className="py-12 bg-sky-400 ">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex  justify-center " >
+                <div>
+                    <div className="flex flex-col  gap-7 md:flex-row items-center justify-center ">
+                        {/* Заголовок секции */}
+                        <h2 className="text-2xl font-extrabold text-white  text-center">
+                            Остались вопросы?
+                        </h2>
+
+
+                        <h2 className="text-3xl font-extrabold text-white  text-center">
+                            Спросите у нашего специалиста!
+                        </h2>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    )
+}
