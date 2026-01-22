682a30d0bec96027416aabbae94fd11494721c6f
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-22 03:15:50 +0200

    feat: add imageData file for image assets

---
+++ src/data/imageData.ts
@@ -0,0 +1,12 @@
+import type {ImageType} from "../../types/imageType.ts";
+
+export const images: ImageType[] = [
+    { id: '1', src: 'src/img/our_works/1.png', alt: 'Photo 1' },
+    { id: '2', src: 'src/img/our_works/2.png', alt: 'Photo 2' },
+    { id: '3', src: 'src/img/our_works/3.png', alt: 'Photo 3' },
+    { id: '4', src: 'src/img/our_works/4.png', alt: 'Photo 4' },
+    { id: '5', src: 'src/img/our_works/5.png', alt: 'Photo 5' },
+    { id: '6', src: 'src/img/our_works/6.png', alt: 'Photo 6' },
+    { id: '7', src: 'src/img/our_works/7.png', alt: 'Photo 7' },
+    { id: '8', src: 'src/img/our_works/8.png', alt: 'Photo 8' },
+];
