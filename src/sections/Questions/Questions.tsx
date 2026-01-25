87bc9a369b3d2ca91a45d1c441c076325f7fabe5
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-25 22:47:48 +0200

    fix: replace form implementation in Promo component with FormInput

--- src/sections/Questions/Questions.tsx
+++ src/sections/Questions/Questions.tsx
@@ -1,41 +1,37 @@
-import {TabsNumber} from "../../components/Modal/Modal.tsx";
-import {handleSubmit} from "../../utils/handleSubmit.ts";
 import {useState} from "react";
+import {TabsNumber} from "../../components/Modal/Modal";
 
 export const Questions = () => {
     const [showTabs, setShowTabs] = useState<boolean>(false);
+
     return (
         <div className="py-12 bg-sky-400 ">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex  justify-center " >
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center">
                 <div>
-                    <div className="flex flex-col  gap-7 md:flex-row items-center justify-center ">
+                    <div className="flex flex-col gap-7 md:flex-row items-center justify-center">
                         {/* Заголовок секции */}
-                        <h2 className="text-2xl font-extrabold text-white  text-center">
+                        <h2 className="text-2xl font-extrabold text-white text-center">
                             Остались вопросы?
                         </h2>
 
-
-                        <button onClick={(e) => {
-                            e.stopPropagation();
-                            setShowTabs(true);
-                        }} className="text-3xl font-extrabold text-white underline text-center">
+                        <button
+                            onClick={() => setShowTabs(true)}
+                            className="text-3xl font-extrabold text-white underline text-center hover:text-gray-100 transition-colors"
+                        >
                             Спросите у нашего специалиста!
                         </button>
                     </div>
                 </div>
-
             </div>
+
             {showTabs && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center">
                     <div className="absolute inset-0 bg-black/50" onClick={() => setShowTabs(false)}/>
                     <div className="relative z-10">
-                        <TabsNumber onClose={() => setShowTabs(false)}  onClick={() => {
-                            handleSubmit();
-                            setShowTabs(false)
-                        }}/>
+                        <TabsNumber onClose={() => setShowTabs(false)} />
                     </div>
                 </div>
             )}
         </div>
-    )
-}
+    );
+};
