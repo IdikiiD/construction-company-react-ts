a07fd36930f7add6bd2dcc2fda3140cbbcf3dad8
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-25 22:46:55 +0200

    fix: update import paths in Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,14 @@
-
-import {Header} from '../../sections/Header/Header.tsx';
-import {Hero} from '../../sections/Hero/Hero.tsx';
-import {Glazing} from "../../sections/Glazing/Glazing.tsx";
-import {Sale} from "../../sections/Sale/Sale.tsx";
-import {Works} from "../../sections/Works/Works.tsx";
-import {Guarantees} from "../../sections/Guarantees/Guarantees.tsx";
-import {Payment} from "../../sections/Payment/Payment.tsx";
-import {Promo} from "../../sections/Promo/Promo.tsx";
-import {Contacts} from "../../sections/Contacts/Contacts.tsx";
-import {Questions} from "../../sections/Questions/Questions.tsx";
-import {Footer} from "../../sections/Footer/Footer.tsx";
+import {Header} from "../../sections/Header/Header";
+import {Hero} from "../../sections/Hero/Hero";
+import {Glazing} from "../../sections/Glazing/Glazing";
+import { Sale } from "../../sections/Sale/Sale";
+import { Works } from "../../sections/Works/Works";
+import {Guarantees} from "../../sections/Guarantees/Guarantees";
+import {Payment} from "../../sections/Payment/Payment";
+import {Promo} from "../../sections/Promo/Promo";
+import { Contacts } from "../../sections/Contacts/Contacts";
+import {Questions} from "../../sections/Questions/Questions";
+import {Footer} from "../../sections/Footer/Footer";
 
 export default function Home() {
 return(
